0bd43696fbbec28e10e45ad1f1ac792564c93d1c
NaMillan <nazarethmillan@gmail.com>
2024-02-20 11:24:03 +0100

    connection database

--- src/index.js
+++ src/index.js
@@ -6,8 +6,34 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+async function getConnection() {
+  const connection= await mysql.createConnection({
+host:'localhost',
+user:'root',
+password:'MiranDa01*', 
+database:'netflix'
+  });
+  await connection.connect ();
+  return connection;
+}
+
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+//endpoints
+
+server.get  ('/api/movies', async (req,res) => {
+  const conex= await getConnection();
+  const sql= 'SELECT * FROM movies';
+  const [results,fields]= await conex.query (sql);
+  console.log (results);
+  console.log (fields);
+  conex.end();
+  res.json({success:true, movies:results });
+});
+
+
+
